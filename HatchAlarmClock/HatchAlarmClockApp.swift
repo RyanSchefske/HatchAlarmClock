0e13f1b2ca54c34a2170b968e5886449e9d9ad49
Ryan Schefske <ryanschefske@gmail.com>
2025-12-09 21:00:43 -0500

    Initial Commit

---
+++ HatchAlarmClock/HatchAlarmClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  HatchAlarmClockApp.swift
+//  HatchAlarmClock
+//
+//  Created by Ryan Schefske on 12/9/25.
+//
+
+import SwiftUI
+
+@main
+struct HatchAlarmClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
